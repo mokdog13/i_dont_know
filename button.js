537a8d8eebea894c004e42da5368ce3a6f869f26
mokdog13 <moku.kau13@gmail.com>
2019-03-07 20:15:10 -0600

    added sliders but not perfect

---
+++ button.js
@@ -0,0 +1,19 @@
+function updateTextInput1(val) {
+    document.getElementById('textInput1').value=val; 
+}
+function updateTextInput2(val) {
+    document.getElementById('textInput2').value=val; 
+}
+function updateTextInput3(val) {
+    document.getElementById('textInput3').value=val; 
+}
+function updateTextInput4(val) {
+    document.getElementById('textInput4').value=val; 
+}
+function updateTextInput5(val) {
+    document.getElementById('textInput').value=val; 
+}
+function updateTextInput6(val) {
+    document.getElementById('textInput').value=val; 
+}
+
